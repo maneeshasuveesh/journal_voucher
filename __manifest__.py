9420dab16dc454ea18dfa5021c86245df3a222fe
planet-odoo <you@example.com>
2019-08-26 13:52:25 +0530

    Manifest changes

--- __manifest__.py
+++ __manifest__.py
@@ -3,14 +3,14 @@
     "version": "12",
     "license": "AGPL-3",
     "author": "",
-    "category": "Sales",
+    "category": "Accounting",
     "website": "",
-    "depends": ["sale"],
+    "depends": ["account"],
     "data": [
   'report/journal_voucher_report.xml',
   'report/journal_voucher_report_template.xml',
   'view/res_company.xml',
-  # 'view/sale.xml'
+
     ],
     "installable": True,
 }
